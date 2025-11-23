bf8f8c10d6792abb52868745bf0f149a9ec45e42
Deepak Kumar <doomerdpk@gmail.com>
2025-11-23 23:41:59 +0530

    Testing CI CD 1

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "/app/secrets/.env" });
+dotenv.config();
 
 // const DATABASE_URL_TEST = process.env.DATABASE_URL_TEST;
 const DATABASE_URL = process.env.DATABASE_URL;
